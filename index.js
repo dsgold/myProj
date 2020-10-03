a27a0102c23243e5e370f263a6d278813dab9963
dsgold <72311610+dsgold@users.noreply.github.com>
2020-10-03 18:56:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views','views')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+    try{
+        await mongoose.connect(
+            'mongodb+srv://admin:admin@cluster0.manwa.mongodb.net/todos', 
+            {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+            }
+        )
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    }   catch (err){
+        console.log('err')
+        throw err
+    }
+}
+
+start()
+
